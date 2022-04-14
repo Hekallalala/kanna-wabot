3b5341c9212a10349a96b958faf40613b1a7fea5
AzizXDBOT <92648503+Azizsapu@users.noreply.github.com>
2022-04-15 06:55:14 +0700

    Delete owneryve.js

--- plugins/owneryve.js
+++
@@ -1,8 +0,0 @@
-let handler = function (m) {
-	this.sendContact(m.chat, '6289518527462', 'Owner AzizGans :)', m)
-}
-
-handler.customPrefix = ['🍭Owner Zeizbot'] 
-handler.command = new RegExp
-
-module.exports = handler
